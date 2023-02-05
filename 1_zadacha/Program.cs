7cc5ce33f65f29698b68e49299ea16a412d16aef
Alyona <molokowa.alyona2010@yandex.ru>
2023-02-05 19:22:22 +0300

    add

---
+++ 1_zadacha/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("vveditee chislo =  ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<1000)
+
+{
+   
+    int a = number/10%10;
+    System.Console.WriteLine (a);
+}
+
+else
+
+{
+
+System.Console.WriteLine("chislo ne trehznachnoe");
+
+} 
---
+++ 2_zadacha/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+System.Console.WriteLine("vvedite chislo" );
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a<99)
+
+{
+    System.Console.WriteLine("tretiey cifry net");
+}
+
+else 
+{
+    while (a>999)
+{
+    a/=10;
+}
+System.Console.WriteLine(a%10);
+}
